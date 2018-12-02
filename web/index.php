13ef9a4d1f329bd082d5bf9a75aa89dad43898bd
Dominik Macher <dominikmacher@live.at>
2018-12-02 18:09:09 +0100

    took new template from w3.css

--- web/index.php
+++ web/index.php
@@ -1,98 +1,124 @@
 <?php
   session_start();
 
-  $sites = array("loewa.php", "list.php", "about.php", "loeschhilfe.php", "login.php", "logout.php", "settings.php", "hydranten.php");
-  $site = $sites[0];
+  $sites = array(
+	0=>array('loewa.php','L&ouml;Wa',true),
+	1=>array('loeschhilfe.php','L&ouml;schhilfe',true),
+	2=>array('list.php','Abgespeicherte Objekte',true),
+	3=>array('hydranten.php','Digitaler Hydrantenplan',true),
+	4=>array('about.php','Info',true),
+	5=>array('logout.php','Logout',false),
+	6=>array('settings.php','Einstellungen',false),
+	7=>array('login.php','Login',false)
+  );
+ 
+  $topic = $sites[0][0];
   if (isset($_GET['id'])) {
-    $site = $sites[$_GET['id']];
+    $topic = $sites[$_GET['id']][0];
   }
+  
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Freiwillige Feuerwehr Karlstetten</title>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Freiwillige Feuerwehr Karlstetten</title>
     <link type="image/x-icon" rel="shortcut icon" href="http://www.feuerwehr-karlstetten.org//cms/templates/feuerwehr-karlstetten/favicon.ico">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-    </style>
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link href="css/bootstrap-print.css" rel="stylesheet" media="print">
-    <link href="css/print.css" rel="stylesheet" media="print">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+	.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+	.fa-anchor,.fa-coffee {font-size:200px}
+	</style>
+	<link href="css/print.css" rel="stylesheet" media="print">
     <link href="css/print-preview.css" rel="stylesheet" type="text/css" media="screen">
 	
-	  <script src="js/jquery-1.7.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.print-preview.js" type="text/javascript" charset="utf-8"></script>
-  </head>
+	<script src="js/jquery-1.7.1.min.js"></script>
+	<script src="js/jquery.print-preview.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
 
-  <body>
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+	
+	<!-- <span class="brand" href="#"><img src="img/logo.png" style="height:19px;margin-top:-5px" alt="FFK Logo">&nbsp;&nbsp;VB-Berechnungs-App</span>
+	-->
+	
+	<!--<a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>-->
+	<?
+	foreach ($sites as $id => $site) {
+		if ($site[2]) {
+			echo '<a href="?id='.$id.'" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white ';
+			if ($site[0]==$topic) {
+				echo 'w3-white';
+			}
+			echo '">'.$site[1].'</a>';
+		}
+	}
+	?>
+  </div>
 
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <div class="nav-search" style="float:right">
-            <ul class="nav">
-            <?php 
-              if (!isset($_SESSION['LOEWA_USER'])) {
-                echo '<li><a href="?id=4"><i class="icon-user"></i> Login</a></li>';
-              }
-              else {
-                //echo '<li><a href="?id=4"><i class="icon-user"></i> Logout</a></li>';
-              ?>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['LOEWA_NAME']; ?><b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="?id=6"><i class="icon-wrench"></i> Passwort ändern</a></li>
-                    <li><a href="?id=5"><i class="icon-off"></i> Abmelden</a></li>
-                  </ul>
-                </li>
-              <?
-              }
-            ?>
-            </ul>
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+  </div>
+</div>
 
-          </div>
-          <span class="brand" href="#"><img src="img/logo.png" style="height:19px;margin-top:-5px" alt="FFK Logo">&nbsp;&nbsp;VB-Berechnungs-App</span>
-          <div class="nav-collapse">
-            <ul class="nav">
-            <?php
-              echo '<li '.($site=="loewa.php" ? 'class="active"' : "").'><a href="?id=0">L&ouml;Wa</a></li>';
-              echo '<li '.($site=="loeschhilfe.php" ? 'class="active"' : "").'><a href="?id=3">L&ouml;schhilfe</a></li>';
-              echo '<li '.($site=="list.php" ? 'class="active"' : "").'><a href="?id=1">Abgespeicherte Objekte</a></li>';
-              echo '<li '.($site=="hydranten.php" ? 'class="active"' : "").'><a href="?id=7">Digitaler Hydrantenplan</a></li>';
-              echo '<li '.($site=="about.php" ? 'class="active"' : "").'><a href="?id=2">Info</a></li>';
-            ?>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+<!-- Header -->
+<!--<header class="w3-container w3-red w3-center" style="padding:128px 16px">
+  <h1 class="w3-margin w3-jumbo">START PAGE</h1>
+  <p class="w3-xlarge">Template by w3.css</p>
+  <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Get Started</button>
+</header>-->
 
-    <div id="content" class="container">
-      <?php 
-        if (file_exists($site)) {
-          include($site);
-        }
-      ?>
-    </div>
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+  <?php 
+	if (file_exists($topic)) {
+	  include($topic);
+	}
+  ?>
+  </div>
+</div>
 
-  </body>
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
+  <div class="w3-xlarge w3-padding-32">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</body>
 </html>
-
-
